76ceb562e86d5c169dce6dc9f4580432739c318c
Kirill <knizamutdinov@outlook.com>
2021-10-21 18:09:27 +1000

    Lesson_2_6_1

--- geekshop/mainapp/views.py
+++ geekshop/mainapp/views.py
@@ -36,12 +36,11 @@ def get_same_products(hot_product):
         
 def main(request):
     title = 'главная'  
-    products = Product.objects.filter(is_active=True, category__is_active=True)[:3]
+    products = Product.objects.filter(is_active=True, category__is_active=True).select_related('category')[:3]
     
     content = {
         'title': title,
         'products': products,
-        'basket': get_basket(request.user),
     }
     
     return render(request, 'mainapp/index.html', content)
