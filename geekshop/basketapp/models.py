dcde76910f2e1f21c56248411d06290ec8e7d54c
Kirill <knizamutdinov@outlook.com>
2021-10-23 13:49:14 +1000

    Lesson_2_7

--- geekshop/basketapp/models.py
+++ geekshop/basketapp/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.conf import settings
 from mainapp.models import Product
+from django.utils.functional import cached_property
 
 
 class Basket(models.Model):
@@ -15,18 +16,14 @@ class Basket(models.Model):
     
     product_cost = property(_get_product_cost)
 
-    def _get_total_quantity(self):
-        "return total quantity for user"
-        _items = Basket.objects.filter(user=self.user)
-        _totalquantity = sum(list(map(lambda x: x.quantity, _items)))
-        return _totalquantity
-        
-    total_quantity = property(_get_total_quantity)
+    @cached_property
+    def get_items_cached(self):
+        return self.user.basket.select_related()
 
-    def _get_total_cost(self):
-        "return total cost for user"
-        _items = Basket.objects.filter(user=self.user)
-        _totalcost = sum(list(map(lambda x: x.product_cost, _items)))
-        return _totalcost
-        
-    total_cost = property(_get_total_cost)
+    def get_total_quantity(self):
+        _items = self.get_items_cached
+        return sum(list(map(lambda x: x.quantity, _items)))
+
+    def get_total_cost(self):
+        _items = self.get_items_cached
+        return sum(list(map(lambda x: x.product_cost, _items)))
